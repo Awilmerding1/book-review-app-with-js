3c091e08b87b819cec9572f0c597e2fe9f0220d9
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-15 15:34:33 -0400

    omniauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login
-  
+
 
   def home
   end
@@ -10,9 +10,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+    else
+      redirect_to '/'
+    end
   end
 
   def show
