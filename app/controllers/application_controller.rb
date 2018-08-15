2d1291ccde5c596b59396d85b624b53551112dfd
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-15 15:06:53 -0400

    user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  def require_login
+
+  end
+
+  def logged_in?
+  end
+
+  def current_user
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
+  before_action :require_login
 
   def home
   end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+
+  
 
 end
