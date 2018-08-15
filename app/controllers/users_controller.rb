3d21e57a6898264235aeea8f048021ad63e2183a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-15 15:02:59 -0400

    css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
+
+  def home
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   Has_many :reviews
 	Has_many :books, through: :reviews
-	Has_many :authors, through :books
+	Has_many :authors, through: :books
 	Has_many :genres, through: :books
 
 end
