028ade4f0c8c012836b7a10883aef42944f71f22
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-16 09:43:27 -0400

    signup form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,8 +20,11 @@ class SessionsController < ApplicationController
   end
 
 	def destroy
-    session.delete :name
-  end
+    if session[:user_id] != nil
+     session.delete :user_id
+    end
+     redirect_to root_url
+	end
 
   private
 
