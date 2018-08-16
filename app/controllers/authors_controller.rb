2b91952acedac9740f3d7fa2f3b6fee196b99545
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-16 11:15:05 -0400

    authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,29 @@
 class AuthorsController < ApplicationController
+
+  def index
+    @authors = Author.all
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to "/authors/#{@author.id}"
+  end
+
+  def show
+    @author = Author.find(params[:id])
+    @books = @author.books
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name)
+  end
+
+
 end
