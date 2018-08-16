2efe965963165909afc68851687782108822aab0
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-16 10:47:47 -0400

    review book methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,14 @@
 class ReviewsController < ApplicationController
 
   def new
+    @review = Review.new(user_id: params[:user_id])
   end
 
   def create
+    @review = Review.new(user_id: current_user.id, book_title: params[:book_title])
+    @review.book
+    @review.save
+    redirect_to "/users/#{current_user.id}"
   end
 
   def show
@@ -12,7 +17,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating, :book_id, :user_id)
+    params.require(:review).permit(:content, :rating, :book_title, :user_id)
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+
+  def book_title=(title)
+    self.book = Book.find_or_create_by(title: title)
+  end
+
+  def book_title
+    self.book ? self.book.title : nil
+  end
+
+
 end
