eeb580bcd7d3d271965b3e5bf180ff88b67773a6
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-20 14:06:44 -0400

    user reviews filter

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   def show
     @review = Review.find(params[:id])
   end
-  
+
   def new
     @review = Review.new(book_id: params[:book_id], user_id: current_user.id)
     @book = Book.find(params[:book_id])
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.save
-    redirect_to "/book/#{@review.book_id}/reviews/#{@review.id}"
+    redirect_to "/books/#{@review.book_id}/reviews/#{@review.id}"
   end
 
 
