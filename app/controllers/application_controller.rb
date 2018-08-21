8f51d4e135d3fce38833fc4f3e91fe4f1349b638
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-21 11:24:23 -0400

    fixed current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-     @user ||= User.find(session[:user_id])
+     @user = User.find(session[:user_id])
    end
   end
 
