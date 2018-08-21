5b6f91a648f0ef4bc7c6a1e4bf716d9ef0748d54
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-21 10:04:15 -0400

    edit reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
-  validates :content, presence: true, uniqueness: true
-  validates :rating, inclusion: { in: 1..5}, presence: true, uniqueness: true
+  validates :content, presence: true
+  validates :rating, inclusion: { in: 1..5}, presence: true
 
   def book_title=(title)
     self.book = Book.find_or_create_by(title: title)
