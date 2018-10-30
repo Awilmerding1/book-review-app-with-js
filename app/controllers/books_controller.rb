882d17d58bc1572dd072f276bc32c753e8afaa19
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-30 12:39:08 -0400

    removed edit book route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,13 +34,13 @@ class BooksController < ApplicationController
     end
 
     def edit
-      book = Book.find(params[:id])
-      if book.book_edit?
-        @book = book
-      else
-        flash[:notice] = "You may not edit this book."
-        redirect_to book_path(book)
-      end
+      @book = Book.find(params[:id])
+      # if book.book_edit?
+        # @book = book
+      # else
+      #   flash[:notice] = "You may not edit this book."
+      #   redirect_to book_path(book)
+      # end
     end
 
     def update
